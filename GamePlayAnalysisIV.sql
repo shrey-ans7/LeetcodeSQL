96cd0a334f37810576c4198ada5b58e5483a0d5b
Shreyans117 <shreyans.magdum@gmail.com>
2024-07-14 00:45:12 -0700

    Update GamePlayAnalysisIV.sql

--- GamePlayAnalysisIV.sql
+++ GamePlayAnalysisIV.sql
@@ -12,4 +12,22 @@ SELECT
 FROM 
     min_date m
 LEFT JOIN 
-    Activity a ON a.player_id = m.player_id AND a.event_date = DATEADD(DAY, 1, m.event_date);
+    Activity a ON a.player_id = m.player_id AND a.event_date = m.event_date + INTERVAL '1 day';
+
+
+
+-- WITH min_date AS (
+--     SELECT
+--         player_id,
+--         MIN(event_date) AS event_date
+--     FROM
+--         Activity 
+--     GROUP BY
+--         player_id
+-- )
+-- SELECT 
+--     ROUND(COUNT(a.event_date) * 1.0 / COUNT(m.event_date), 2) AS fraction
+-- FROM 
+--     min_date m
+-- LEFT JOIN 
+--     Activity a ON a.player_id = m.player_id AND a.event_date = DATEADD(DAY, 1, m.event_date);
